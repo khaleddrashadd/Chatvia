6aef5bf5c95f422e036bf4bae9bf7c20fcbe0da2
khaleddrashadd <khaledrashad111@gmail.com>
2023-06-25 03:53:05 +0300

    added meta description and logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,4 @@
-import add from './add.png';
 import addAvatar from './addAvatar.png';
-import attach from './attach.png';
-import cam from './cam.png';
-import more from './more.png';
 import img from './img.png';
 
-
-export { add, addAvatar, attach, cam, more, img };
+export { addAvatar, img };
